c5a347ea1f1bd802a156e0c042c86b17f2151036
EmanuelDRusu <emanueldrusu@gmail.com>
2023-05-24 18:46:09 +0300

    ITL-8 Add the title
    - added "To do list" title

--- todoapp/src/components/Header.vue
+++ todoapp/src/components/Header.vue
@@ -2,6 +2,7 @@
 <script setup lang="ts">
 import DeleteTasks from './DeleteTasks.vue'
 
+<<<<<<< develop
 const emit = defineEmits(['deleteTasks'])
 
 function deleteTasks() {
@@ -12,6 +13,11 @@ function deleteTasks() {
   <div class="title-cleartasks">
     <h1>To do list</h1>
     <DeleteTasks @deleteTasks="deleteTasks"></DeleteTasks>
+=======
+<template>
+  <div class="header">
+    <h1 class="header-title">To do list</h1>
+>>>>>>> ITL-8 Add the title
   </div>
 </template>
 
